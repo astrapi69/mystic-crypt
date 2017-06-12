ca2ec186668d074158c1981f9912dab476737f7d
Tatjana Kopalova <tatjana19@users.noreply.github.com>
2017-06-12 09:44:46 +0200

    Create KeystoreType.java

---
+++ crypt-data/src/main/java/de/alpharogroup/crypto/algorithm/KeystoreType.java
@@ -0,0 +1,19 @@
+package de.alpharogroup.crypto.algorithm;
+
+/**
+ * The enum {@link KeystoreType} for algorithm that are used to create {@link KeyStore} objects.
+ */
+public enum KeystoreType {
+	
+	/** The enum constant for DSA algorithm. */
+	DSA, 
+
+	/** The enum constant for JKS algorithm. */
+	JKS, 
+
+	/** The enum constant for PKCS12 algorithm. */
+	PKCS12, 
+
+	/** The enum constant for JCEKS algorithm. */
+	JCEKS;
+}
