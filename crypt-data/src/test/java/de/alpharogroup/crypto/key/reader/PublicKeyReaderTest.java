22072965dec9b1c3695a3d331e8b6e2a6fdbca02
Tatjana Kopalova <tatjana19@users.noreply.github.com>
2017-06-13 18:54:56 +0200

    Create PublicKeyReaderTest.java

--- crypt-data/src/test/java/de/alpharogroup/crypto/key/reader/PublicKeyReaderTest.java
+++ crypt-data/src/test/java/de/alpharogroup/crypto/key/reader/PublicKeyReaderTest.java
@@ -24,7 +24,17 @@
  */
 package de.alpharogroup.crypto.key.reader;
 
+import java.io.File;
+import java.security.PublicKey;
+import java.security.Security;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
+
+import de.alpharogroup.crypto.key.PublicKeyExtensions;
+import de.alpharogroup.crypto.provider.SecurityProvider;
+import de.alpharogroup.file.search.PathFinder;
 
 /**
  * Test class for {@link PublicKeyReader}.
