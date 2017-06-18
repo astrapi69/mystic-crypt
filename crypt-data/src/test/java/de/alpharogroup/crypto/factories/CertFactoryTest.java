8cd11a1dbe47f9f08497af96d8461bf4366d1233
Asterios Raptis <asterios.raptis@gmx.net>
2017-06-18 15:06:50 +0200

    unit test extended

--- crypt-data/src/test/java/de/alpharogroup/crypto/factories/CertFactoryTest.java
+++ crypt-data/src/test/java/de/alpharogroup/crypto/factories/CertFactoryTest.java
@@ -33,7 +33,9 @@ import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.Security;
 import java.security.cert.X509Certificate;
-import java.time.Instant;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.ZoneId;
 import java.util.Date;
 
 import org.bouncycastle.asn1.x500.X500Name;
@@ -111,8 +113,9 @@ public class CertFactoryTest
 		final String issuer = "CN=Test issue";
 		final String signatureAlgorithm = HashAlgorithm.SHA256.getAlgorithm() + CryptConst.WITH
 			+ KeyPairGeneratorAlgorithm.RSA.getAlgorithm();
-		final Date start = new Date(System.currentTimeMillis());
-		final Date end = new Date(System.currentTimeMillis() + (1000L * 60 * 60 * 24 * 100));
+
+		final Date start = Date.from(LocalDate.of(2017, Month.JANUARY, 1).atStartOfDay(ZoneId.systemDefault()).toInstant());
+		final Date end = Date.from(LocalDate.of(2027, Month.JANUARY, 1).atStartOfDay(ZoneId.systemDefault()).toInstant());
 		final BigInteger serialNumber = randomSerialNumber();
 		// create certificate
 		final X509Certificate cert = CertFactory.newX509Certificate(publicKey, privateKey,
@@ -141,8 +144,8 @@ public class CertFactoryTest
 		final KeyPair keyPair = KeyPairFactory.newKeyPair(KeyPairGeneratorAlgorithm.RSA, 2048);
 		final X500Name issuer = new X500Name("CN=Issuer of this certificate");
 		final BigInteger serial = BigInteger.ONE;
-		final Date notBefore = Date.from(Instant.now());
-		final Date notAfter = Date.from(Instant.now().plusSeconds(60 * 60 * 24 * 365 * 5));
+		final Date notBefore = Date.from(LocalDate.of(2017, Month.JANUARY, 1).atStartOfDay(ZoneId.systemDefault()).toInstant());
+		final Date notAfter = Date.from(LocalDate.of(2027, Month.JANUARY, 1).atStartOfDay(ZoneId.systemDefault()).toInstant());
 		final X500Name subject = new X500Name("CN=Subject of this certificate");
 		final String signatureAlgorithm = "SHA1withRSA";
 		final X509Certificate cert = CertFactory.newX509CertificateV1(keyPair, issuer, serial, notBefore, notAfter, subject,
