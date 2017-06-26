2045756754771ea3a942d857687a8e26a9e5eeda
Jacqueline Morin <jacqueline.morin@gmx.net>
2017-06-26 19:11:14 +0200

    Update HasherTest.java

--- crypt-core/src/test/java/de/alpharogroup/crypto/sha/HasherTest.java
+++ crypt-core/src/test/java/de/alpharogroup/crypto/sha/HasherTest.java
@@ -30,7 +30,6 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
-import java.util.Arrays;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -42,6 +41,7 @@ import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 
 import de.alpharogroup.crypto.algorithm.HashAlgorithm;
+import de.alpharogroup.random.RandomExtensions;
 
 /**
  * Test class for {@link Hasher}.
@@ -52,67 +52,6 @@ public class HasherTest
 	/** The Constant logger. */
 	private static final Logger logger = Logger.getLogger(HasherTest.class.getName());
 
-	/**
-	 * Test method for {@link Hasher#hash(byte[], String, HashAlgorithm, Charset)}
-	 *
-	 * @throws NoSuchAlgorithmException
-	 *             is thrown if instantiation of the MessageDigest object fails.
-	 */
-	@Test
-	public void testHashByteArray() throws NoSuchAlgorithmException
-	{
-		final Charset charset = Charset.forName("UTF-8");
-		final String password = "abcdefghijklmnopqrst";
-		final String newInsertPassword = "abcdefghijklmnopqrst";
-		final String salt = new String(Hasher.getRandomSalt(8, charset), charset);
-		final HashAlgorithm hashAlgorithm = HashAlgorithm.SHA_512;
-		final  byte[] expected = Hasher.hash(password.getBytes(), salt, hashAlgorithm, charset);
-		final  byte[] actual = Hasher.hash(newInsertPassword.getBytes(), salt, hashAlgorithm, charset);
-
-		AssertJUnit.assertTrue(expected.length == actual.length);
-		AssertJUnit.assertTrue("'expected' should be equal with 'actual'.", Arrays.equals(expected, actual));
-	}
-
-	/**
-	 * Test method for {@link Hasher#hash(String, String, HashAlgorithm, Charset)}
-	 *
-	 * @throws NoSuchAlgorithmException
-	 *             is thrown if instantiation of the MessageDigest object fails.
-	 */
-	@Test
-	public void testHash() throws NoSuchAlgorithmException
-	{
-		final Charset charset = Charset.forName("UTF-8");
-		final String password = "abcdefghijklmnopqrst";
-		final String newInsertPassword = "abcdefghijklmnopqrst";
-		final String salt = new String(Hasher.getRandomSalt(8, charset), charset);
-		final HashAlgorithm hashAlgorithm = HashAlgorithm.SHA_512;
-		final String expected = Hasher.hash(password, salt, hashAlgorithm, charset);
-		final String actual = Hasher.hash(newInsertPassword, salt, hashAlgorithm, charset);
-
-		AssertJUnit.assertTrue("'expected' should be equal with 'actual'.",
-			expected.equals(actual));
-	}
-
-	/**
-	 * Test method for {@link Hasher#hashAndBase64(String, String, HashAlgorithm, Charset)}
-	 *
-	 * @throws NoSuchAlgorithmException
-	 *             is thrown if instantiation of the MessageDigest object fails.
-	 */
-	@Test
-	public void testHashAndBase64() throws NoSuchAlgorithmException
-	{
-		final Charset charset = Charset.forName("UTF-8");
-		final String password = "secret";
-		final String newInsertPassword = "secret";
-		final String salt = new String(Hasher.getRandomSalt(8, charset), charset);
-		final HashAlgorithm hashAlgorithm = HashAlgorithm.SHA_512;
-		final String expected = Hasher.hashAndBase64(password, salt, hashAlgorithm, charset);
-		final String actual = Hasher.hashAndBase64(newInsertPassword, salt, hashAlgorithm, charset);
-		AssertJUnit.assertTrue("'expected' should be equal with 'actual'.",
-			expected.equals(actual));
-	}
 
 	/**
 	 * Test method for {@link Hasher#hashAndHex(String, String, HashAlgorithm, Charset)}
@@ -142,7 +81,7 @@ public class HasherTest
 		final Charset charset = Charset.forName("UTF-8");
 		final String password = "xxx";
 		final String newInsertPassword = "xxx";
-		final String salt = new String(Hasher.getRandomSalt(8, charset), charset);
+		final String salt = new String(RandomExtensions.getRandomSalt(8, charset), charset);
 		final HashAlgorithm hashAlgorithm = HashAlgorithm.SHA_512;
 		final String expected = Hasher.hashAndHex(password, salt, hashAlgorithm, charset);
 		final String actual = Hasher.hashAndHex(newInsertPassword, salt, hashAlgorithm, charset);
