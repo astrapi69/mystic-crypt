12f1ade61d5bdde40e99773f054675ffc8ab9cdd
Asterios Raptis <asterios.raptis@gmx.net>
2017-12-14 00:00:56 +0100

    created new launch script

--- crypt-core/src/main/java/de/alpharogroup/crypto/core/AbstractCryptor.java
+++ crypt-core/src/main/java/de/alpharogroup/crypto/core/AbstractCryptor.java
@@ -92,8 +92,9 @@ public abstract class AbstractCryptor<C, K> implements Serializable, Cryptor
 		throws InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException,
 		NoSuchPaddingException, InvalidAlgorithmParameterException, UnsupportedEncodingException
 	{
-		Check.get().notNull(model, "model");
-		Check.get().notNull(model.getKey(), "model.getKey()");
+		Check.get()
+		.notNull(model, "model")
+		.notNull(model.getKey(), "model.getKey()");
 		this.model = model;
 		onInitialize();
 	}
